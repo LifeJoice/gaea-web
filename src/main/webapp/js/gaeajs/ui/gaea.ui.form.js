6bf9fbe7ca9b9ec7c547ab58a3c382b0eeec03d8
iverson <lifejoice@outlook.com>
2016-02-20 17:20:14 +0800

    【新功能】 增加通用表单样式和js。可以自动根据表单的内容自动调整大小。

---
+++ src/main/webapp/js/gaeajs/ui/gaea.ui.form.js
@@ -0,0 +1,25 @@
+/**
+ * 基于RequireJS的模块化重构。让依赖更清晰，更简单。
+ * 对通用表单的处理。目前主要是做一些样式的处理。
+ * Created by iverson on 2016-2-19 20:27:30
+ */
+require(["jquery","underscore",'gaeajs-common-utils-ajax','gaeajs-common-utils-validate'],function ($,_,gaeaAjax,gaeaValid) {
+    var gaeaForm = $(".gaea-form");
+    /**
+     * 对通用表单的样式处理
+     */
+    gaeaForm.children(".row").each(function (key, value) {
+        var fieldBlocks = $(this).children(".field-block");
+        var fieldsCount = fieldBlocks.length;
+        // 如果是一行就一个输入项，则自动把输入框延展到最后
+        if(fieldsCount == 1){
+            fieldBlocks.removeClass("field-block").addClass("one-field-block");
+            fieldBlocks.children(".fieldvalue").addClass("one-field");
+            fieldBlocks.find("input").addClass("one-field");
+            fieldBlocks.find("textarea").addClass("one-field");
+            // 加上margin，避免输入框过长挤到下面了
+            var width = fieldBlocks.find(".fieldname").css("width");
+            fieldBlocks.find(".fieldvalue").css("margin-left",width);
+        }
+    })
+})
