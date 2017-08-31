e48491780b4d690cc50e12997abc98d0c4821461
Iverson <lifejoice@outlook.com>
2017-08-31 16:25:45 +0800

    id同步改为Long。

--- src/main/java/org/gaea/security/jo/UserJO.java
+++ src/main/java/org/gaea/security/jo/UserJO.java
@@ -1,24 +1,21 @@
 package org.gaea.security.jo;
 
-import org.gaea.security.domain.Role;
-
 import java.io.Serializable;
-import java.util.List;
 
 /**
  * 这个主要用于用户的信息（缓存、返回前端等）
  * Created by iverson on 2017/8/17.
  */
 public class UserJO implements Serializable {
-    private String id;
+    private Long id;
     private String name;
     private String loginName;
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
